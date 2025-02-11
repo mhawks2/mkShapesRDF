64a9026f2280508b299b20394604e7aa469e2072
NTrevisani <nicolo.trevisani@cern.ch>
2025-02-11 14:25:13 +0100

    testing fix to run on portal1

--- mkShapesRDF/processor/framework/Sites_cfg.py
+++ mkShapesRDF/processor/framework/Sites_cfg.py
@@ -1,6 +1,16 @@
+import os
 
 Sites = {
-    "eosTmpWorkDir": "USEDAS", ### Optional to redirect the input samples
-    "eosDir"       : "/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/",
-    "rediretor"    : "root://cmsxrootd.fnal.gov/", # xrootd-cms.infn.it. # cmsxrootd.fnal.gov
+
+    'cern' : {
+        "eosTmpWorkDir": "'USEDAS'", ### Optional to redirect the input samples
+        "eosDir"       : "/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/",
+        "rediretor"    : "root://cmsxrootd.fnal.gov/", # xrootd-cms.infn.it. # cmsxrootd.fnal.gov
+    },
+
+    'kit' : {
+        "eosTmpWorkDir": "'/tmp/'", # + os.getlogin() + "/'", ### Optional to redirect the input samples
+        "eosDir"       : "/ceph/ntrevisa/HWWNano/",
+        "rediretor"    : "root://cmsxrootd.fnal.gov/", # xrootd-cms.infn.it. # cmsxrootd.fnal.gov
+    }
 }
