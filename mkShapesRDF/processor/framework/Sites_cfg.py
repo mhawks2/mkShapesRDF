568b9e7f17bc11a65580f103d7bea42ca35b62f3
NTrevisani <nicolo.trevisani@cern.ch>
2025-03-10 16:49:44 +0100

    fixing redirector in sites.py

--- mkShapesRDF/processor/framework/Sites_cfg.py
+++ mkShapesRDF/processor/framework/Sites_cfg.py
@@ -5,12 +5,12 @@ Sites = {
     'cern' : {
         "eosTmpWorkDir": "'USEDAS'", ### Optional to redirect the input samples
         "eosDir"       : "/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/",
-        "rediretor"    : "root://cmsxrootd.fnal.gov/", # xrootd-cms.infn.it. # cmsxrootd.fnal.gov
+        "redirector"    : "root://cmsxrootd.fnal.gov/", # xrootd-cms.infn.it. # cmsxrootd.fnal.gov
     },
 
     'kit' : {
         "eosTmpWorkDir": "'/tmp/'", # + os.getlogin() + "/'", ### Optional to redirect the input samples
         "eosDir"       : "/ceph/ntrevisa/HWWNano/",
-        "rediretor"    : "root://cmsxrootd.fnal.gov/", # xrootd-cms.infn.it. # cmsxrootd.fnal.gov
+        "redirector"    : "root://cmsxrootd.fnal.gov/", # xrootd-cms.infn.it. # cmsxrootd.fnal.gov
     }
 }
