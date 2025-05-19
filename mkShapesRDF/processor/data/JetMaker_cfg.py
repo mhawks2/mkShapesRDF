3c6af5797afe0a9de11abe48b220f37d78abd0e2
squinto5 <sebastiano.quinto@cern.ch>
2025-05-19 16:11:54 +0200

    fix indentation

--- mkShapesRDF/processor/data/JetMaker_cfg.py
+++ mkShapesRDF/processor/data/JetMaker_cfg.py
@@ -15,7 +15,7 @@ JetMakerCfg = {
     'Full2022EEv12': {
         "vetomap": frameworkPath + "/processor/data/jsonpog-integration/POG/JME/2022_Summer22EE/jetvetomaps.json.gz",
         "vetokey": "Summer22EE_23Sep2023_RunEFG_V1",
-	    "JEC": "Summer22EE_22Sep2023_V2_MC",
+	"JEC": "Summer22EE_22Sep2023_V2_MC",
         "JEC_data" : ["Summer22EE_22Sep2023_RunE_V2_DATA", 'Summer22EE_22Sep2023_RunF_V2_DATA', 'Summer22EE_22Sep2023_RunG_V2_DATA'],
         "JER": "Summer22EE_22Sep2023_JRV1_MC",
         "jet_jerc" : frameworkPath + "/processor/data/jsonpog-integration/POG/JME/2022_Summer22EE/jet_jerc.json.gz",
@@ -24,7 +24,7 @@ JetMakerCfg = {
     'Full2023v12': {
         "vetomap": frameworkPath + "/processor/data/jsonpog-integration/POG/JME/2023_Summer23/jetvetomaps.json.gz",
         "vetokey": "Summer23Prompt23_RunC_V1",
-	    "JEC": "Summer23Prompt23_V1_MC",
+	"JEC": "Summer23Prompt23_V1_MC",
         "JEC_data" : ["Summer23Prompt23_RunCv123_V1_DATA", "Summer23Prompt23_RunCv4_V1_DATA"],
         "JER": "Summer23Prompt23_RunCv1234_JRV1_MC",
         "jet_jerc" : frameworkPath + "/processor/data/jsonpog-integration/POG/JME/2023_Summer23/jet_jerc.json.gz",
@@ -33,7 +33,7 @@ JetMakerCfg = {
     'Full2023BPixv12': {
         "vetomap": frameworkPath + "/processor/data/jsonpog-integration/POG/JME/2023_Summer23BPix/jetvetomaps.json.gz",
         "vetokey": "Summer23BPixPrompt23_RunD_V1",
-	    "JEC": "Summer23BPixPrompt23_V1_MC",
+	"JEC": "Summer23BPixPrompt23_V1_MC",
         "JEC_data" : "Summer23BPixPrompt23_RunD_V1_DATA",
         "JER": "Summer23BPixPrompt23_RunD_JRV1_MC",
         "jet_jerc" : frameworkPath + "/processor/data/jsonpog-integration/POG/JME/2023_Summer23BPix/jet_jerc.json.gz",
